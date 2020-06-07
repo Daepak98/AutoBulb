49e378a8f47b197b1eea35ead0eec9922fcbe704
daepak98 <bunch.of.food@gmail.com>
2020-06-07 15:32:43 -0400

    Comments and Code Cleanup

--- bulb_controller/bulb_controller.ino
+++ bulb_controller/bulb_controller.ino
@@ -1,15 +1,21 @@
-int relayPin = 8;
-int pirPin = 7;
-const int baud_rate = 9600;
-int read = 0;
-long waittime = (long) 20*1000;
+int relayPin = 8; // The pin to turn on the bulb
+int pirPin = 7; // The pin to read the PIR sensor from
+const int baud_rate = 9600; // Baud Rate for Serial Output
+long waittime = (long)30; // Wait time for sensor warm up
+
+/**
+ * serialInput - Listens for serial input, and returns the integer value passed through it
+ **/
 int serialInput()
 {
-  while (!Serial.available()) {}
+  while (!Serial.available());
   int value = Serial.parseInt();
   return value;
 }
 
+/**
+ * pirInput - Reads, and returns, the digital value passed from the PIR sensor
+ **/
 int pirInput()
 {
   int pirRead = digitalRead(pirPin);
@@ -17,21 +23,32 @@ int pirInput()
   return pirRead;
 }
 
+/**
+ * callFunction - Calls the callback function passed in as a parameter, and writes it's resulting value to the `relayPin`
+ *  @param callback - A function that returns an int and accepts no arguments
+ **/
 void callFunction(int (*callback)())
 {
   int highOrNot = callback();
   digitalWrite(relayPin, highOrNot);
 }
 
+/**
+ * setup - Arduino's setup function. Sets up the pins for I/O, as well as sets up Serial output.
+ **/
 void setup()
 {
   pinMode(pirPin, INPUT);
   pinMode(relayPin, OUTPUT);
   Serial.begin(baud_rate);
-  Serial.println("Wait 20 sec before using the sensor");
-  delay(waittime);
+  String wait_msg = "Wait " + String(waittime) + "sec before using the sensor";
+  Serial.println(wait_msg);
+  delay(waittime*1000);
 }
 
+/**
+ * loop - Arduino's loop runction. Calls the requested "source" function that determines what turns the `relayPin` on and off
+**/
 void loop()
 {
   callFunction(&pirInput);
