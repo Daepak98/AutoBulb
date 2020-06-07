3fdfa489d7a5eb87fd638fed699d337332f964a3
daepak98 <bunch.of.food@gmail.com>
2020-06-07 15:07:48 -0400

    Working Setup - For now - Still having issues with the sensor going LOW when motion is there

--- bulb_controller/bulb_controller.ino
+++ bulb_controller/bulb_controller.ino
@@ -1,13 +1,38 @@
-int y = 0;
+int relayPin = 8;
+int pirPin = 7;
 const int baud_rate = 9600;
+int read = 0;
+long waittime = (long) 20*1000;
+int serialInput()
+{
+  while (!Serial.available()) {}
+  int value = Serial.parseInt();
+  return value;
+}
+
+int pirInput()
+{
+  int pirRead = digitalRead(pirPin);
+  Serial.println(pirRead);
+  return pirRead;
+}
+
+void callFunction(int (*callback)())
+{
+  int highOrNot = callback();
+  digitalWrite(relayPin, highOrNot);
+}
 
 void setup()
 {
-    Serial.begin(baud_rate);
+  pinMode(pirPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+  Serial.begin(baud_rate);
+  Serial.println("Wait 20 sec before using the sensor");
+  delay(waittime);
 }
 
 void loop()
 {
-    Serial.print(y++);
-    Serial.print("\n");
+  callFunction(&pirInput);
 }
