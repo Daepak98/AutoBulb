a898d564e1d4aa6bb98c2d7c2214b10fd73ac91a
daepak98 <bunch.of.food@gmail.com>
2022-01-12 12:25:48 -0500

    Added a driver to manage the 3 parts of the application

---
+++ driver.py
@@ -0,0 +1,36 @@
+import numpy as np
+import time
+import threading
+
+from light_controller import RectMatrixLightController
+from position_reporter import CameraPositionReporter
+from hw_interface import LightHardwareInterface, PiLEDHardwareInterface
+
+if __name__ == "__main__":
+    # layout = np.ones((3, 4))
+    layout = np.array([[1, 1, 1, 1],
+                       [2, 2, 2, 2],
+                       [3, 3, 3, 3]])
+    image_points = np.loadtxt(
+        "./resources/image_points.csv", dtype=np.float32, delimiter=",")
+    world_points = np.loadtxt(
+        "./resources/world_points.csv", dtype=np.float32, delimiter=",")
+
+    lc = RectMatrixLightController(layout)
+    pr = CameraPositionReporter("/home/pi/AutoBulb/resources/ryan_video.mp4",
+                                layout,
+                                image_points,
+                                world_points)
+    lm = PiLEDHardwareInterface(lc.lights, [17, 27, 22], 10)
+
+    positions = pr.get_positions()
+    while positions != False:
+        lc.update_lights(*positions)
+        lm.listen_to_light_model(lc.lights)
+    # if positions:
+        print(positions)
+        print(lc.lights)
+        print(lm.delay_timer)
+        print()
+        positions = pr.get_positions()
+        # time.sleep(0.5)
